461366128494d35660f7622f7c97bbb8c1a4414d
Noot <test@localhost>
2023-05-23 21:55:09 -0400

    feat: small fix on last commit

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,10 +4,10 @@ import ProjectCard from './ProjectCard';
 function Projects() {
   return (
     <div className='flex min-h-screen snap-start justify-center bg-dark-100 bg-opacity-75'>
-      <div className='mx-4 flex max-w-xl grow flex-col pb-32 pt-12 lg:mx-auto'>
+      <div className='mx-4 max-w-xl grow flex-col pb-32 pt-12 lg:mx-auto'>
         <h1 className='font-Oswald text-6xl'>Projects</h1>
         <br />
-        <div className='flex flex-col justify-center'>
+        <div className='flex flex-col'>
           {projects.map((project, index) => (
             <ProjectCard key={index} project={project} />
           ))}
