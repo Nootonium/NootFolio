80bb605cee3eaac88d6a20cc07c875264af6d2ea
Noot <test@localhost>
2023-06-03 17:39:58 -0400

    feat: project cards mostly done

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTheme } from '../hooks/ThemeContext';
 import { Project } from '../types';
 import TechBadge from './TechBadge';
@@ -6,6 +6,19 @@ import TechBadge from './TechBadge';
 function ProjectCard({ project }: { project: Project }) {
   const { theme } = useTheme();
   const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
+  const descriptionRef = useRef();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (descriptionRef.current && !descriptionRef.current.contains(event.target)) {
+        setIsDescriptionExpanded(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
   const cardClasses = {
     light: 'bg-neutral-200 ring-1 ring-neutral-300 -ring-offset-1',
     dark: 'bg-neutral-800',
@@ -13,42 +26,43 @@ function ProjectCard({ project }: { project: Project }) {
   };
 
   return (
-    <div className={`my-2 flex w-full flex-col items-center p-4 shadow-xl ${cardClasses[theme]}`}>
+    <div
+      className={`my-2 flex w-full flex-col items-center px-2 py-2 shadow-xl sm:px-4 ${cardClasses[theme]}`}
+    >
       <img
         src={project.image_url}
         alt={project.title}
         className='mb-4 aspect-video w-full rounded object-cover'
       />
       <div className='w-full text-left'>
-        <h2 className='mb-2 text-xl font-bold'>{project.title}</h2>
-        <p className={`${!isDescriptionExpanded ? 'line-clamp-2' : ''}`}>{project.description}</p>
-        <button
-          onClick={() => setIsDescriptionExpanded(!isDescriptionExpanded)}
-          className='mb-2 text-blue-500 hover:text-blue-800'
-        >
-          {isDescriptionExpanded ? 'Read less' : 'Read more'}
-        </button>
+        <h2 className='mb-2 font-bold'>{project.title}</h2>
+        <div ref={descriptionRef}>
+          <p className={`min-h-12 ${!isDescriptionExpanded ? 'line-clamp-2' : ''}`}>
+            {project.description}
+          </p>
+          <button
+            onClick={() => setIsDescriptionExpanded(!isDescriptionExpanded)}
+            className='mb-2 text-blue-500 hover:text-blue-800'
+          >
+            {isDescriptionExpanded ? 'Read less' : 'Read more'}
+          </button>
+        </div>
         <div className='mb-2 space-x-2'>
           {project.tech_stack.map((tech, index) => (
             <TechBadge key={index} tech={tech} />
           ))}
         </div>
-        <div className='flex flex-col justify-center md:flex-row md:justify-between'>
+        <div className='flex flex-row items-center justify-between'>
           <a
-            href={project.link}
+            href={project.link ? project.link : project.github}
             target='_blank'
             rel='noreferrer'
-            className='mb-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 md:mb-0'
+            className='rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700'
           >
             View Project
           </a>
-          <a
-            href={project.github}
-            target='_blank'
-            rel='noreferrer'
-            className='rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700'
-          >
-            GitHub
+          <a href={project.github} target='_blank' rel='noreferrer' className='pr-2'>
+            <i className='fab fa-github fa-2x'></i>
           </a>
         </div>
       </div>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,84 +2,14 @@ import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 
 function Skills() {
-  const skillsRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const currentRef = skillsRef.current;
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000,
-    );
-    const renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    if (currentRef) {
-      currentRef.appendChild(renderer.domElement);
-    }
-
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-
-    canvas.width = 512;
-    canvas.height = 256;
-
-    if (context) {
-      context.font = '40px Arial';
-      context.fillStyle = 'white';
-      context.fillText('May the Force be with you', 50, 50);
-    }
-
-    const texture = new THREE.CanvasTexture(canvas);
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-    const geometry = new THREE.PlaneGeometry(8, 4);
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.rotation.x = -Math.PI / 2; // tilt the plane
-
-    scene.add(mesh);
-    camera.position.z = 5;
-
-    let animationId: number;
-
-    const animate = function () {
-      animationId = requestAnimationFrame(animate);
-
-      mesh.position.y += 0.01; // move the text upwards
-      mesh.position.z += 0.01; // move the text away
-
-      renderer.render(scene, camera);
-    };
-
-    console.log(canvas.toDataURL());
-
-    animate();
-
-    // Clean up
-    return () => {
-      if (animationId) {
-        cancelAnimationFrame(animationId);
-      }
-
-      // remove mesh from scene
-      scene.remove(mesh);
-      geometry.dispose();
-      material.dispose();
-      texture.dispose();
-
-      // dispose of the renderer
-      renderer.dispose();
-
-      // remove the renderer's dom element from the HTML
-      if (currentRef) {
-        currentRef.removeChild(renderer.domElement);
-      }
-    };
-  }, []);
-
   return (
-    <div className='h-screen snap-start'>
-      <div ref={skillsRef}></div>
+    <div className='flex h-screen w-screen snap-start bg-black'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pretium vitae felis ut euismod.
+        Quisque nec dui sem. Cras et lectus sapien. Nam interdum dignissim velit ac mattis. Integer
+        ante diam, efficitur ac accumsan quis, lobortis at ligula. Suspendisse et orci ut lectus
+        luctus ultricies nec nec massa. Donec efficitur tincidunt arcu in aliquet.
+      </p>
     </div>
   );
 }
