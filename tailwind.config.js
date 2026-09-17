331df8f397560541796c7a421f186f7bb85909d9
nootonium <ldaniel358@gmail.com>
2026-09-17 16:24:19 -0400

    chore: remove obsolete Tailwind theme plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,11 @@ export default {
         JetBrainsMono: ['JetBrains Mono', 'monospace'],
         OpenSans: ['Open Sans', 'sans-serif'],
       },
-      backgroundColor: {
-        rainbow: 'bg-gradient-to-r from-red-500 via-yellow-500 to-cyan-500',
-      },
       textColor: {},
       colors: {},
     },
   },
-  plugins: [require('daisyui'), require('@headlessui/tailwindcss')],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: ['light', 'dark', 'black'],
   },
