833348bd9d35b84bc3e83e5eec9e0f068036c805
nootonium <ldaniel358@gmail.com>
2026-09-17 16:47:08 -0400

    temp change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -12,9 +11,5 @@ export default {
       textColor: {},
       colors: {},
     },
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: ['light', 'dark', 'black'],
   },
 };
